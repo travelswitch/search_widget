3ce610d04d85c238ec9a36a888a08b7b7f55c5d0
Lubna Khan <lubna.infohybrid@gmail.com>
2025-07-15 20:52:33 +0530

    Naming convention & config file updated

--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
-var config = (function() {
+var config = (function () {
     var settings = {
         development: {
             adminApiUrl: "https://adminapi.dev.futuretravelplatform.com/api",
-            hotelApiUrl: "https://hotelapi.dev.futuretravelplatform.com/api"
+            hotelApiUrl: "https://hotelapi.dev.futuretravelplatform.com/api",
+            domain: 'https://retinassr.dev.futuretravelplatform.com'
         }
     };
 
     // Default to 'development'. A more robust solution could check the hostname.
-    var environment = 'development'; 
+    var environment = 'development';
 
     return settings[environment];
 })();
